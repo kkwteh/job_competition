90ff87cdbb8faa18e75f428393958198719a1b0b
Kevin Teh <kkwteh@gmail.com>
2013-03-15 17:43:37 -0700

    fixed untracted files

--- kaggle_job_model.py
+++ kaggle_job_model.py
@@ -3,37 +3,62 @@ from continuize_feature import *
 from word_feature_enhance import *
 from sklearn.ensemble import RandomForestRegressor
 import re
+import sys
+import datetime
 
-train = pd.read_csv("Train_rev1.csv")
-valid = pd.read_csv("Valid_rev1.csv")
+def make_model_and_predict(train_file, test_file):
+    train = pd.read_csv(train_file)
+    valid = pd.read_csv(test_file)
 
-title_words = count_words_in_column(train, "Title")
-for word, count in title_words.items():
-    add_appearance_count_feature(train, word, "Title")
-    add_appearance_count_feature(valid, word, "Title")
+    number_of_word_features = 10
+    title_words = count_words_in_column(train, "Title")
+    key_count_pairs = title_words.items()
+    key_count_pairs.sort(key=lambda (k,v): -v)
+    for word, count in key_count_pairs[:number_of_word_features]:
+        add_appearance_count_feature(train, word, "Title")
+        add_appearance_count_feature(valid, word, "Title")
 
-group_features = ["LocationNormalized", "Category", "Company", "SourceName"]
+    group_features = ["LocationNormalized", "Category", "Company", "SourceName"]
 
-for f in group_features:
-    continuize_feature(train, valid, f, "SalaryNormalized")
+    for f in group_features:
+        continuize_feature(train, valid, f, "SalaryNormalized")
 
-feature_columns = train.columns[12:]
+    feature_columns = train.columns[12:]
 
-feature=train[feature_columns]
-label=train.SalaryNormalized
-clf = RandomForestRegressor()
-clf.fit(feature, label)
+    feature=train[feature_columns]
+    label=train.SalaryNormalized
+    clf = RandomForestRegressor()
+    clf.fit(feature, label)
 
-valid_salary_predict = clf.predict(valid[feature_columns])
-valid["SalaryNormalized_Predict"] = valid_salary_predict
+    valid_salary_predict = clf.predict(valid[feature_columns])
+    valid["SalaryNormalized_Predict"] = valid_salary_predict
 
-prelim_filename = 'kaggle_job_predict.csv'
+    date_string = re.sub("[ :.]", "", str(datetime.datetime.now()))
+    prelim_filename = 'raw_predict' + date_string + '.csv'
+    final_filename = 'predict' + date_string + '.csv'
 
-with open(prelim_filename,'wb') as f:
-    valid[["Id","SalaryNormalized_Predict"]].to_csv(f)
+    with open(prelim_filename,'wb') as f:
+        valid[["Id","SalaryNormalized_Predict"]].to_csv(f)
 
 
-with open(prelim_filename,'rb') as f:
-    with open('refined_kaggle_job_predict.csv','wb') as g:
-        for line in f.xreadlines():
-            g.write(re.sub('^[^,]*,', '', line))
+    with open(prelim_filename,'rb') as f:
+        with open(final_filename,'wb') as g:
+            for line in f.xreadlines():
+                g.write(re.sub('^[^,]*,', '', line))
+
+def main():
+    """
+    Standard main function boilerplate to accept command line arguments
+    """
+    args = sys.argv[1:]
+
+    if not args:
+        print 'usage: train_file test_file'
+        sys.exit(1)
+
+    train_file = args[0]
+    test_file = args[1]
+    make_model_and_predict(train_file, test_file)
+
+if __name__ == '__main__':
+    main()
