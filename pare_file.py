1c5a81c7ad0938d235d811e74ba7b2d5fe842178
Kevin Teh <kkwteh@gmail.com>
2013-03-16 16:48:59 -0700

    added split_file method to pare_file module

--- pare_file.py
+++ pare_file.py
@@ -12,14 +12,15 @@ import sys
 
 def pare_file(data_file, percent):
     """
-    This method does the paring.
+    This method pares down the file, keeping the header and writes the result
+    to "mini_" + data_file.
     Args:
     data_file: name of the file to be pared down
     percent: percentage of rows to sample (roughly)
     """
 
     mini_data_file = "mini_" + data_file
-    with open(data_file, 'r') as f:
+    with open(data_file, 'rb') as f:
         with open(mini_data_file, 'wb') as g:
             line = f.readline()
             g.write(line)
@@ -27,9 +28,32 @@ def pare_file(data_file, percent):
                 if random.random() < percent:
                     g.write(line)
 
+def split_file(data_file, percent=0.1):
+    """This method splits the data file into two pieces, a training set,
+    and a validation set, keeping the header. The training set is written to
+    "train_part_" + data_file and the validation set is written to
+    "validation_part_" + data_file
+    Args:
+    data_file: name of the file
+    percent: approximate percent of file to be written to validation set.
+    """
+    train_data_file = "train_part_" + data_file
+    validation_data_file = "validation_part_" + data_file
+    with open(data_file, 'rb') as f:
+        with open(train_data_file, 'wb') as g:
+            with open(validation_data_file, 'wb') as h:
+                line = f.readline()
+                g.write(line)
+                h.write(line)
+                for line in f.xreadlines():
+                    if random.random() < percent:
+                        g.write(line)
+                    else:
+                        h.write(line)
 def main():
     """
-    Standard main function boilerplate to accept command line arguments
+    Standard main function boilerplate to accept command line arguments and runs
+    pare_file method
     """
     args = sys.argv[1:]
 
